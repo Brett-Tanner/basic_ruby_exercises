e0ce7b01280a73180721e4067b5fc1da2ed46ce1
Brett <bretttanner171@gmail.com>
2022-05-24 22:23:53 +0900

    Start pseudocode

---
+++ substrings.rb
@@ -0,0 +1,9 @@
+# get the word you're searching in
+
+# get the array of substrings
+
+# check if there's a match between the word and each substring
+
+# count the number of matches for each substring
+
+# output a hash that lists each subtring with a match and how many times it matched
