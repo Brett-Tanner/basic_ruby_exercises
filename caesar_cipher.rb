bb61b13b6a8e8443b480ea1b313253b54eafbf16
Brett Tanner <bretttanner171@gmail.com>
2022-05-24 20:45:18 +0900

    Basic functionality achieved

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,29 +2,29 @@
 puts "Please enter the message you wish to convert"
 input_string = gets.chomp
 
+# check they're valid characters 
 unless input_string.ascii_only?
     puts "Only ASCII characters are accepted"
-    return 1
+    return
 end
 
 # get cipher key (check its an integer)
 puts "What would you like to shift by?"
-shift_key = gets.chomp
+shift_key = gets.chomp.to_i
 
-# turn the string into an array of chars FIXME: just turns them into 0s
-char_array = input_string.chars
+# convert the string to an array of ASCII values
+ascii_array = []
+input_string.each_codepoint { |char| ascii_array.push(char)}
 
-puts char_array
 
-# convert each char to its ASCII code
-ascii_array = char_array.map { |element| element.to_i}
-
-puts ascii_array
-
-# add (.map?) the cipher key to each ASCII code (while making sure to wrap if you go out of bounds, also preserve case)
+# add (.map?) the cipher key to each ASCII code (TODO: while making sure to wrap if you go out of bounds, also preserve case)
+shifted_ascii = ascii_array.map { |value| value + shift_key}
 
 # convert back to chars
+shifted_chars = shifted_ascii.map { |value| value.chr}
 
 # join the array of chars to a string
+shifted_string = shifted_chars.join
 
 # put the resulting string
+puts "Your ciphered message is #{shifted_string}"
