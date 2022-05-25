624b790ac411ebf9923a65c97a1ef7e52d49dfa1
Brett <bretttanner171@gmail.com>
2022-05-25 14:36:04 +0900

    Start creating search method

--- substrings.rb
+++ substrings.rb
@@ -7,10 +7,12 @@ puts "Input substrings to search for, separated by only a space"
 substrings = gets.chomp
 substring_array = substrings.split(" ") 
 
-# check if there's a match between the word and each substring
-
-
-
-# count the number of matches for each substring (.include?)?
+# check if there's a match between the word and each substring (reduce)? and count the number of matches for each substring (.include?)? FIXME: converting string to an int here?
+hash_of_matches = substring_array.reduce(Hash.new{0}) do |accumulator, value|
+    if word.include?(value)
+        accumulator[value] += 1
+    end
+end
 
 # output a hash that lists each subtring with a match and how many times it matched
+p hash_of_matches
