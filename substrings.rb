46ab20af75b5ffbf1af0980048cb6450ef6588bb
Brett Tanner <bretttanner171@gmail.com>
2022-05-25 16:35:41 +0900

    Allow to search for multiple instance of substring

--- substrings.rb
+++ substrings.rb
@@ -7,11 +7,12 @@ puts "Input substrings to search for, separated by only a space"
 substrings = gets.chomp
 substring_array = substrings.split(" ") 
 
-# check if there's a match between the word and each substring (reduce)? and count the number of matches for each substring (.include?)? FIXME: converting string to an int here?
-hash_of_matches = substring_array.reduce(Hash.new{0}) do |accumulator, value|
-    if word.include?(value)
-        accumulator[value] += 1
+# check if each substring is included in the word
+hash_of_matches = substring_array.reduce(Hash.new(0)) do |accumulator, value|
+    word.scan(value) do |match|
+        accumulator[match] += 1
     end
+    accumulator
 end
 
 # output a hash that lists each subtring with a match and how many times it matched
