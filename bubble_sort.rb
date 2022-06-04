e783e340a1e59f8f20a47369b4ad3e2f06699933
Brett Tanner <bretttanner171@gmail.com>
2022-06-04 13:40:28 +0900

    Decide on each_with_index for sorting

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,17 +2,32 @@
 puts "Enter numbers to be sorted, separated by a space"
 input_array = gets.chomp.split(" ") 
 
-# check inputs are numbers
+# check inputs are numeric
 unless input_array.all? {|value| Float(value) rescue false}
     puts "Only numbers can be sorted"
     return
 end
 
-# convert the input values to numbers
+# convert the input values from strings to floats
 input_array.map! {|value| value.to_f}
 
 # compare two values and swap them if right is smaller
+# track number of swaps made
+num_of_swaps = 0
+sorted_array = input_array.each_with_index do |value, index|
+    next_value = input_array[index + 1]
+    if value > next_value
+        temp = next_value
+        next_value = value
+        value = temp
+        num_of_swaps++ 
+    end
+    
+end
+
 
 # check if the array is sorted??
+
+
 
 # if not sorted run again
