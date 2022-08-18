da23b20a4c01d0382297762dbbd4097bcd5f8be4
Brett Tanner <bretttanner171@gmail.com>
2022-08-18 12:37:34 +0900

    Test #apply_key and #to_string

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -25,32 +25,31 @@ class Cypher
     def to_ascii(message)
         message.codepoints
     end
+
+    def apply_key(ascii_array)
+        ascii_array.map do |value|
+            if (value > 64 && value < 91) || (value > 96 && value < 123)
+                shifted_value = value + @key
+                # wrap for capitals
+                if shifted_value > 90 && shifted_value < 97
+                    shifted_value - 90 + 64
+                # wrap for lowercase
+                elsif shifted_value > 122
+                    shifted_value - 122 + 96
+                else
+                    shifted_value
+                end
+            else
+                value
+            end
+        end
+    end
+
+    def to_string(shifted_array)
+        shifted_array.map {|value| value.chr}.join
+    end
 end
 
-# # add the cipher key to each ASCII code
-# shifted_ascii = ascii_array.map do |value|
-#     # only shift alphabetical characters
-#     if (value > 64 && value < 91) || (value > 96 && value < 123)
-#         shifted_value = value + shift_key
-#         # wrap for capitals
-#         if shifted_value > 90 && shifted_value < 97
-#             shifted_value - 90 + 64
-#         # wrap for lowercase
-#         elsif shifted_value > 122
-#             shifted_value - 122 + 96
-#         else
-#             shifted_value
-#         end
-#     else
-#         value
-#     end
-# end
-
-# # convert back to chars
-# shifted_chars = shifted_ascii.map { |value| value.chr}
-
-# # join the array of chars to a string
-# shifted_string = shifted_chars.join
 
 # # put the resulting string
 # puts "Your ciphered message is: #{shifted_string}"
--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -12,29 +12,29 @@ RSpec.describe Cypher do
 
         describe "#get_message" do
             
-            it "Gets message from user" do
+            it "gets message from user" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string)
                 expect(subject.input_message).to_not be_nil
             end
     
-            it "Ensures message is a string" do
+            it "ensures message is a string" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string)
                 expect(subject.input_message).to be_an_instance_of String
             end
     
-            it "Doesn't accept non-ASCII characters" do
+            it "doesn't accept non-ASCII characters" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return("日本人\n", def_string)
                 expect(subject.input_message).to be_ascii_only
             end
         end
 
         describe "#get_key" do
-            it "Gets key from user" do
+            it "gets key from user" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return("10")
                 expect(subject.key).to_not be_nil
             end
 
-            it "Returns an integer" do
+            it "returns an integer" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return("10")
                 expect(subject.key).to be_an_instance_of Integer
             end
@@ -45,15 +45,66 @@ RSpec.describe Cypher do
     context "While ciphering" do
 
         describe "#to_ascii" do
-            it "Returns an array" do
+            it "returns an array" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
                 expect(subject.to_ascii("hi")).to be_an_instance_of Array
             end
             
-            it "Of message's ASCII values" do
+            it "converts to message's ASCII values" do
                 allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
                 expect(subject.to_ascii(subject.input_message)).to eq([72, 105, 32, 116, 104, 101, 114, 101, 32, 102, 114, 105, 101, 110, 100])
             end
+        end
+
+        describe "#apply_key" do
+            it "returns an array" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.apply_key([65, 66])).to be_an_instance_of Array
+            end
+
+            it "returns an array of eql length to input array" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.apply_key([65, 66]).length).to eq(2)
+            end
+
+            it "adds the key to each value" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.apply_key([65, 66])).to eq([75, 76])
+            end
+
+            it "wraps around when end of alphabet is reached" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.apply_key([85, 121])).to eq([69, 105])
+            end
+        end
+
+        describe "#to_string" do
+            it "returns a string" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.to_string([102, 111, 111])).to eq("foo")
+            end
+
+            it "returns a string of same length as input array" do
+                allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+                expect(subject.to_string([102, 112, 113]).length).to eq(3)
+            end
+        end
+    end
+
+    context "result" do
+        
+        it "wraps around when end of alphabet is reached" do
+            allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+            
+        end
+        
+        it "maintains case" do
+            allow_any_instance_of(Cypher).to receive(:gets).and_return(def_string, "10")
+            
+        end
+
+        it "returns ciphered message of same length as input message" do
+            
         end
     end
 end
